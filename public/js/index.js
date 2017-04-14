72c26163e06653f942ad6cc00061275527197d36
xujie <xujie1@jiandan100.cn>
2017-04-14 18:17:12 +0800

    登录，注册效果切换

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,13 @@
 /**
  * Created by ludongdong on 2017/4/14.
  */
+var  loginBtn = $('loginDiv');
+var  registerBtn = $('registerDiv');
+$('#loginBtn').click = function(){
+  registerBtn.hide();
+  loginBtn.show();
+};
+$('#registerBtn').click = function(){
+  loginBtn.hide();
+  registerBtn.show();
+};
